d4046ca3a61fcecbe8f7253769216ffa60a26640
pramudithanavodya <your-github-email@example.com>
2026-05-06 10:55:43 +0530

    Folder structure updated

---
+++ src/main/java/com/abcinstitute/student_management/StudentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.abcinstitute.student_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentManagementApplication.class, args);
+        System.out.println("✅ ABC Institute running at: http://localhost:8080");
+    }
+}
